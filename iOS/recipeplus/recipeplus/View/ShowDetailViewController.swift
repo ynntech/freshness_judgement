4b806e950c0d517f543105f2f65b7a45dd1cd359
yushi <jointapps.pony@gmail.com>
2020-02-22 19:37:19 +0900

    レシピとか具材表示できるようになった！

--- iOS/recipeplus/recipeplus/View/ShowDetailViewController.swift
+++ iOS/recipeplus/recipeplus/View/ShowDetailViewController.swift
@@ -8,11 +8,39 @@
 
 import Foundation
 import UIKit
-class  ShowDetailViewController: UIViewController {
+class  ShowDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var recipe_name: UILabel!
+        
+        var guidance:[Recipe.Response.Recipes.Guidance]!
+        var data:Recipe!
+        var name:String = ""
+        var processArray:[String] = []
+        var count = 0
         override func viewDidLoad() {
                 super.viewDidLoad()
- 
+            
+            recipe_name.text = name
+            for i in 0...guidance!.count-1{
+                processArray.append("\(guidance[i].process!)")
             }
-
+            print("processArray\(processArray)")
+        
+        }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("tableviewの関数1やで")
+        return guidance.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        print("guidance[indexPath.row].process は　\(String(describing: guidance[indexPath.row].process))")
+        cell.textLabel!.text = processArray[indexPath.row]
+ 
+        return cell
+    }
+}
+
