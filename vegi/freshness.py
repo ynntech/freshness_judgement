6d3157bff25641dd15e97d68e08bbdb6ad56ef7f
yusuke_takahashi <43262348+YU-SUKETAKAHASHI@users.noreply.github.com>
2020-02-24 02:14:35 +0900

    野菜分類, 鮮度判定, others分類, S3に保存 ができるようになりました～

--- vegi/freshness.py
+++ vegi/freshness.py
@@ -1,46 +1,44 @@
-"""
-コメントアウトを外すとそのままkerasの鮮度判定モデルが動くはず
-requirements.txtに以下を追記
-Keras==2.2.5
-tensorflow==1.15.2
-"""
-import os
-from flask import Flask, request, jsonify, abort
-from werkzeug.utils import secure_filename
 import numpy as np
 from PIL import Image
+from flask import Flask, request, jsonify, abort
+
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
-"""
+
 from keras.models import load_model
 from keras.utils import CustomObjectScope
 from keras.initializers import glorot_uniform
 import tensorflow as tf
-"""
+
 import base64
 from io import BytesIO
 import json
+import datetime
+import boto3
+from botocore.exceptions import ClientError
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config["JSON_SORT_KEYS"] = False
+#s3 = boto3.resource('s3').Bucket("recipeplus-s3") # 画像保存用S3
+
 
 #英語の昇順
-vegis = ["きゃべつ", "にんじん", "ねぎ"]
-freshness_ = [0,50,100] # bad, good, verygood
-"""
-graph = tf.get_default_graph()
-"""
-#鮮度モデルとの結合部分
-"""
-modelpath = {"きゃべつ" : "weights/cabbage_model.h5" , "にんじん" : "weights/carrot_model.h5" , "ねぎ" : "weights/green_onion_model.h5"}
-with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
-    fresh_models = {vegi : load_model(modelpath[vegi]) for vegi in vegis}
-"""
+veges = ["キャベツ", "にんじん", "ねぎ"]
+freshness_int = [0,50,100] # bad, good, verygood
+freshness_str = ["bad","good","verygood"]
 
-# load Pytorch model
+
+graph = tf.compat.v1.get_default_graph()
+#鮮度モデルの定義
+modelpath = {"キャベツ" : "weights/cabbage_model.h5" , "にんじん" : "weights/carrot_model.h5" , "ねぎ" : "weights/green_onion_model.h5"}
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+    fresh_models = {vege : load_model(modelpath[vege]) for vege in veges}
+
+
+# 野菜分類モデルの定義
 vegi_judge = models.resnet18(pretrained=True)
 num_ftrs = vegi_judge.fc.in_features
 vegi_judge.fc = nn.Sequential(nn.Linear(num_ftrs, 256),
@@ -49,9 +47,11 @@ vegi_judge.fc = nn.Sequential(nn.Linear(num_ftrs, 256),
                               nn.Linear(256,64),
                               nn.ReLU(),
                               nn.Dropout(),
-                              nn.Linear(64,3))
+                              nn.Linear(64,3),
+                              nn.Softmax(1))
 vegi_judge.load_state_dict(torch.load("weights/vegi08.pth", map_location=torch.device('cpu')))
 vegi_judge.eval()
+
 
 class MyDataset(torch.utils.data.Dataset):
 
@@ -86,19 +86,29 @@ def pillowImage2torchDataLoader(img):
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
     ])
-
     dataset = MyDataset([img], [0], transform=transform)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
- 
     return dataloader
 
 
-def pillowimage2keras(img):
-    img = img.resize((224,224))
-    img = np.array(img)
+def pillowimage2keras(img, vege):
+    if vege == "にんじん":
+        input_shape = 256
+    else:
+        input_shape = 224
+    img = img.resize((input_shape,input_shape))
+    img = np.array(img)/255
     img = np.expand_dims(img, axis=0)
-    #img = img[:, :, ::-1]
     return img
+
+
+def pillowImage2s3(pillow_image, vege, freshness):
+    pillow_image.save("tmp.jpg")
+    now = datetime.datetime.now()
+    try:
+        s3.upload_file("tmp.jpg", vege+"/"+freshness+"/"+str(now)[:19]+".jpg")
+    except ClientError as e:
+        logging.error(e)
 
 
 @app.route('/test', methods=['GET'])
@@ -116,46 +126,47 @@ def freshness():
             })
 
     if request.method == 'POST':
-        print(request.is_json)
-        print(request.headers)
-
-        if "file" not in request.get_json(): 
-            return jsonify({
-                "status" : "OK"
-                })
-
+        print("accept!")
+        
+        #base64_str = request.files["file"].read()
         json_data = request.get_json() 
         base64_str = json_data["file"]# base64strを取得
      
         pillow_img = base64string2pillowimage(base64_str) # base64strをPilloImageに変換
      
-        # vegi phese　野菜分類
+        # vege phese　野菜分類
         dataloader = pillowImage2torchDataLoader(pillow_img)
         for inputs, labels in dataloader:
             output = vegi_judge(inputs)
-            print(output)
             _, predict = torch.max(output, 1)
-            vegi = vegis[predict]
-        
 
-        """
-        # freshness phese
-        with graph.as_default():
-            img_keras = pillowimage2keras(pillow_img)
-            fn_judge = fresh_models[vegi]
-            predicted = np.argmax(fn_judge.predict(img_keras)[0])
-            freshness = freshness_[predicted]
-            """     
-        
-        # 鮮度判定モデルは割愛してるので定数。
-        freshness = 100
-        item_class = "vegetable" if vegi in vegis else "other"
+            output_detached = output.detach().numpy()[0] # 推論結果の数値部分をnumpy arrayで取り出す
+            predict_num = predict.numpy()[0]
+            if output_detached[predict_num] >= 0.6: # 出力の最高値が0.6以上のときに正しく分類できたとみなし、鮮度判定にうつる
+                vege = veges[predict]
 
+                # freshness phese 鮮度判定
+                with graph.as_default():
+                    img_keras = pillowimage2keras(pillow_img, vege) 
+                    fn_judge = fresh_models[vege]
+                    result = fn_judge.predict(img_keras)[0]
+                    predict = np.argmax(result)
+                    freshness = result[1]*50 + result[2]*100
+                
+            else: #　出力の最高値が0.6未満のときは未知の野菜とみなして鮮度判定をしない
+                vege = "others"
+                predict = 2
+                freshness = 100        
+
+        print("vege : {}, pred_rate : {} || freshness : {}, pred_rate : {}".format(vege, output_detached, freshness, result))
+
+       # pillowImage2s3(pillow_img, vege, freshness_str[predict], item_class) # AWS S3に画像を野菜別鮮度別（3段階）で保存
+        
         return jsonify({
             "status": "OK",
             "vegi_info":{
-                "item_class": item_class,
-                "name": vegi,
+                "item_class": "vegetable",
+                "name": vege,
                 "freshness": freshness,
                 "amount": "1"
             }
@@ -163,5 +174,6 @@ def freshness():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080)) # herokuでPORT番号固定するのに一手間かかるため、サーバから指定される開いてるポートを使ってる
-    app.run(host="0.0.0.0", port=port)
+    app.debug = True
+    #app.run(host="0.0.0.0", port="8888")
+    app.run(host="0.0.0.0",port="3000")
