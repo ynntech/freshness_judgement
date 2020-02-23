d57c87b145ac79eeeb3ecab4be94a078eda831ee
yushi <jointapps.pony@gmail.com>
2020-02-23 14:21:48 +0900

    レシピ改行できるようになった！

--- iOS/recipeplus/recipeplus/View/ShowDetailViewController.swift
+++ iOS/recipeplus/recipeplus/View/ShowDetailViewController.swift
@@ -10,8 +10,10 @@ import Foundation
 import UIKit
 class  ShowDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var recipe_name: UILabel!
-        
-        var guidance:[Recipe.Response.Recipes.Guidance]!
+    
+    
+
+    var guidance:[Recipe.Response.Recipes.Guidance]!
         var data:Recipe!
         var name:String = ""
         var processArray:[String] = []
@@ -23,7 +25,11 @@ class  ShowDetailViewController: UIViewController, UITableViewDelegate, UITableV
             for i in 0...guidance!.count-1{
                 processArray.append("\(guidance[i].process!)")
             }
+            
             print("processArray\(processArray)")
+            
+           
+   
         
         }
     
@@ -38,8 +44,10 @@ class  ShowDetailViewController: UIViewController, UITableViewDelegate, UITableV
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         // セルに表示する値を設定する
         print("guidance[indexPath.row].process は　\(String(describing: guidance[indexPath.row].process))")
+        
         cell.textLabel!.text = processArray[indexPath.row]
- 
+        cell.textLabel!.adjustsFontSizeToFitWidth = true
+        cell.textLabel!.numberOfLines = 0
         return cell
     }
 }
